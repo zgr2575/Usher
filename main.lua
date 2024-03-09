fb6de57a6bc19125e5552ecf3d2db384a777a1e1
zgr2575 <62474113+zgr2575@users.noreply.github.com>
2024-03-09 12:57:12 -0500

    Create main.lua

---
+++ main.lua
@@ -0,0 +1,26 @@
+-- Introducing Usher (V1)
+
+local v =0.1;
+-- Alpha Release
+local dskey = 3232
+local multiban = false -- Set this to true if you want to ban them from all using your USHER URI.
+local url = "https://yoururl.com" -- HTTPS Services must be on!!!
+
+local function usher(user) 
+local exploit
+
+if exploit == "fly" then
+local humanoid = game.workspace:FindFirstChild(user).Humanoid 
+if humanoid.CFrame == workspace.baseplate.CFrame +5 then
+    print("User is not flying")
+    return false
+else
+
+user:kick("Exploit Detected, USHER V1")  -- THIS IS BEING CODED INF VSCODE AND I HAVENT CODED LUA IN 2 YEARS
+end
+end
+
+end
+
+
+    --OMG IM DOING JS IMA DIE
